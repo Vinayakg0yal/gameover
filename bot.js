c6d4f910a338937c4a830b18fddb953970e5a352
jerrygithub37 <58998335+jerrygithub37@users.noreply.github.com>
2019-12-18 06:39:53 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,14 +11,24 @@ bot.on('message', message =>{
 
     if(message.author.id != "656542594406416391" && message.channel.id === "656658556011020288"){
         if(msg.startsWith('>', 0)){
-            if(msg =    == prefix + "yt" && message.channel.id === "656658556011020288"){
-                message.channel.send('CHANNEL LINK!!! https://www.youtube.com/channel/UCs9mR_zW4UJotnbv5pf3wwQ ') // Sends a message to the channel, with the content
-            }  
+            if(msg === prefix + "yt" && message.channel.id === "656658556011020288"){
+                message.channel.send('CHANNEL LINK!! https://www.youtube.com/channel/UCs9mR_zW4UJotnbv5pf3wwQ ') // Sends a message to the channel, with the content
+            }
+           else if(msg === prefix + "insta" && message.channel.id === "656658556011020288"){
+                message.channel.send('INSTAGRAM!! https://www.instagram.com/subham_30/ ') // Sends a message to the channel, with the content
+            }
+            else if(msg === prefix + "Discord" && message.channel.id === "656658556011020288"){
+                message.channel.send('Discord Link!! https://discord.gg/g3B9fuR ') // Sends a message to the channel, with the content
+            }
             else{
-                message.channel.send("Hey, I only understand some right now. More will be added when my creator is free. :) ")
+                message.channel.send("Hey, I only understand some commands right now. More will be added when my creator is free. :) ")
             }
         }
-       
+        else{
+            message.delete();
+            message.channel.send(sender + " this bot only accepts commands which starts with '>' dot.");
+        }
     }
 });
+
 bot.login(process.env.BOT_TOKEN)
